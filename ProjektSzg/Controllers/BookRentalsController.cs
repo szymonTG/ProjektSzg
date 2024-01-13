57e1e3711ed8aa0dac6ea993612997db8cbf1b6b
szymonTG <117183035+szymonTG@users.noreply.github.com>
2024-01-13 21:16:51 +0100

    added visibikity change defined by role

--- ProjektSzg/Controllers/BookRentalsController.cs
+++ ProjektSzg/Controllers/BookRentalsController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -10,6 +11,7 @@ using ProjektSzg.Models;
 
 namespace ProjektSzg.Controllers
 {
+    [Authorize(Roles ="Admin,User")]
     public class BookRentalsController : Controller
     {
         private readonly ApplicationDbContext _context;
--- ProjektSzg/Controllers/UsersController.cs
+++ ProjektSzg/Controllers/UsersController.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -10,6 +12,7 @@ using ProjektSzg.Models;
 
 namespace ProjektSzg.Controllers
 {
+    [Authorize(Roles = "Admin")]
     public class UsersController : Controller
     {
         private readonly ApplicationDbContext _context;
