fc818a669cb47f103c5e021d86db8d85976cc039
szymonTG <117183035+szymonTG@users.noreply.github.com>
2024-01-13 19:57:52 +0100

    Added annotations to Users and Books

--- ProjektSzg/Models/Books.cs
+++ ProjektSzg/Models/Books.cs
@@ -1,13 +1,22 @@
-﻿namespace ProjektSzg.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProjektSzg.Models
 {
     public class Books
     {
+        [Key]
         public int bookId { get; set; }
-
+        [Required (ErrorMessage ="The title is required!")]
+        [MaxLength(55)]
+        [MinLength(1)]
         public string title { get; set; }
-
+        [Required(ErrorMessage = "The author is required!")]
+        [MaxLength(75)]
+        [MinLength(10)]
         public string author { get; set; }
-
+        [Required(ErrorMessage = "The ISBN is required!")]
+        [MaxLength(13)]
+        [MinLength(13)]
         public string isbn { get; set; }
 
         public ICollection<BookRentals>? BookRentals { get; set; }
--- ProjektSzg/Models/Users.cs
+++ ProjektSzg/Models/Users.cs
@@ -1,11 +1,21 @@
-﻿namespace ProjektSzg.Models
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace ProjektSzg.Models
 {
     public class Users
     {
+        [Key]
         public int userId { get; set; }
+        [Required(ErrorMessage = "The user first name is required!")]
+        [MaxLength(100)]
+        [MinLength(1)]
         public string firstName { get; set; }
-
+        [Required(ErrorMessage = "The user surname name is required!")]
+        [MaxLength(100)]
+        [MinLength(1)]
         public string surname { get; set; }
+        [Required(ErrorMessage = "The user telephone number is required!")]
 
         public int telNumber { get; set; }
 
