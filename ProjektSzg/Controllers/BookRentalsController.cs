04efbac8bc349ebcbc46b3ceae7146f4d6292bdf
szymonTG <117183035+szymonTG@users.noreply.github.com>
2024-01-13 20:21:18 +0100

    created local db and BookRentalsController

---
+++ ProjektSzg/Controllers/BookRentalsController.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ProjektSzg.Data;
+using ProjektSzg.Models;
+
+namespace ProjektSzg.Controllers
+{
+    public class BookRentalsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public BookRentalsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: BookRentals
+        public async Task<IActionResult> Index()
+        {
+              return _context.TableBookRentals != null ? 
+                          View(await _context.TableBookRentals.ToListAsync()) :
+                          Problem("Entity set 'ApplicationDbContext.TableBookRentals'  is null.");
+        }
+
+        // GET: BookRentals/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.TableBookRentals == null)
+            {
+                return NotFound();
+            }
+
+            var bookRentals = await _context.TableBookRentals
+                .FirstOrDefaultAsync(m => m.rentalId == id);
+            if (bookRentals == null)
+            {
+                return NotFound();
+            }
+
+            return View(bookRentals);
+        }
+
+        // GET: BookRentals/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: BookRentals/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("rentalId,startDate,endDate,userId,bookId")] BookRentals bookRentals)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(bookRentals);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(bookRentals);
+        }
+
+        // GET: BookRentals/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.TableBookRentals == null)
+            {
+                return NotFound();
+            }
+
+            var bookRentals = await _context.TableBookRentals.FindAsync(id);
+            if (bookRentals == null)
+            {
+                return NotFound();
+            }
+            return View(bookRentals);
+        }
+
+        // POST: BookRentals/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("rentalId,startDate,endDate,userId,bookId")] BookRentals bookRentals)
+        {
+            if (id != bookRentals.rentalId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(bookRentals);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!BookRentalsExists(bookRentals.rentalId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(bookRentals);
+        }
+
+        // GET: BookRentals/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.TableBookRentals == null)
+            {
+                return NotFound();
+            }
+
+            var bookRentals = await _context.TableBookRentals
+                .FirstOrDefaultAsync(m => m.rentalId == id);
+            if (bookRentals == null)
+            {
+                return NotFound();
+            }
+
+            return View(bookRentals);
+        }
+
+        // POST: BookRentals/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.TableBookRentals == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.TableBookRentals'  is null.");
+            }
+            var bookRentals = await _context.TableBookRentals.FindAsync(id);
+            if (bookRentals != null)
+            {
+                _context.TableBookRentals.Remove(bookRentals);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool BookRentalsExists(int id)
+        {
+          return (_context.TableBookRentals?.Any(e => e.rentalId == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ ProjektSzg/Data/Migrations/20240113191236_initial.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProjektSzg.Data.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TableBooks",
+                columns: table => new
+                {
+                    bookId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    title = table.Column<string>(type: "nvarchar(55)", maxLength: 55, nullable: false),
+                    author = table.Column<string>(type: "nvarchar(75)", maxLength: 75, nullable: false),
+                    isbn = table.Column<string>(type: "nvarchar(13)", maxLength: 13, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TableBooks", x => x.bookId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TableEmployees",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    employeeName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    employeeSurname = table.Column<int>(type: "int", nullable: false),
+                    telNumber = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TableEmployees", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TableUsers",
+                columns: table => new
+                {
+                    userId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    firstName = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    surname = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    telNumber = table.Column<int>(type: "int", maxLength: 9, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TableUsers", x => x.userId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TableBookRentals",
+                columns: table => new
+                {
+                    rentalId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    startDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    endDate = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    userId = table.Column<int>(type: "int", nullable: false),
+                    UsersuserId = table.Column<int>(type: "int", nullable: true),
+                    bookId = table.Column<int>(type: "int", nullable: false),
+                    BooksbookId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TableBookRentals", x => x.rentalId);
+                    table.ForeignKey(
+                        name: "FK_TableBookRentals_TableBooks_BooksbookId",
+                        column: x => x.BooksbookId,
+                        principalTable: "TableBooks",
+                        principalColumn: "bookId");
+                    table.ForeignKey(
+                        name: "FK_TableBookRentals_TableUsers_UsersuserId",
+                        column: x => x.UsersuserId,
+                        principalTable: "TableUsers",
+                        principalColumn: "userId");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TableBookRentals_BooksbookId",
+                table: "TableBookRentals",
+                column: "BooksbookId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TableBookRentals_UsersuserId",
+                table: "TableBookRentals",
+                column: "UsersuserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TableBookRentals");
+
+            migrationBuilder.DropTable(
+                name: "TableEmployees");
+
+            migrationBuilder.DropTable(
+                name: "TableBooks");
+
+            migrationBuilder.DropTable(
+                name: "TableUsers");
+        }
+    }
+}
